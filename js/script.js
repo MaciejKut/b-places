72ceb34e77ba43c4048093c9298cf95a1a68144e
MaciejKut <maciej.kutzmann@gmail.com>
2019-01-20 18:48:58 +0100

    Add complete script, html, style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+console.log('Wartość obiektu Slides Data' + slidesData);
+var slides = document.getElementById('template-slide-list').innerHTML;
+console.log('Wartość slides po pobraniu' + slides);
+
+Mustache.parse(slides);
+var listSlides = '';
+
+for (var i = 0; i < slidesData.length; i++) {
+    console.log(slidesData);
+    listSlides += Mustache.render(slides, slidesData[i]);
+}
+
+console.log('Wartość listSlides po pętli ' + listSlides);
+console.log(typeof (listSlides));
+
+var results = document.getElementById('slidesContainer');
+results.innerHTML = listSlides;
+
+console.log(results);
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
     // options
